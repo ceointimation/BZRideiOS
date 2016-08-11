b6812f8378410127edb36e049dffb3370be46bd7
Sooraj <sooraj@users-Mac-mini.local>
2016-08-11 16:59:43 +0530

    Initial Commit

---
+++ BZRide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BZRide
+//
+//  Created by Sooraj on 8/11/16.
+//  Copyright (c) 2016 Sooraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
